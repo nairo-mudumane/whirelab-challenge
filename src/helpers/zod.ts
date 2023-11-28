5e207db774aef30aa614e3c3f5b05fd7d9fcf4f9
nairo-mudumane <nairomudumane@outlook.com>
2023-11-28 16:18:17 +0200

    fix: zod errors format

--- src/helpers/zod.ts
+++ src/helpers/zod.ts
@@ -2,8 +2,8 @@ import { ZodError } from "zod";
 
 function throwNewZodError(error: ZodError) {
   const { issues } = error;
-  const { code, message } = issues[0];
-  const errMessage = `ERR_${code.toUpperCase()}: ${message}`;
+  const { message, path } = issues[0];
+  const errMessage = `${path}: ${message}`;
 
   throw new Error(errMessage);
 }
